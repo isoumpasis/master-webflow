7ee60bc90a5533aee27948bb0f29f58f51e5b223
isoumpasis <wweacountss@gmail.com>
2023-07-23 01:09:32 +0300

    webflow push

--- home.js
+++ home.js
@@ -503,7 +503,7 @@ function setNextFocusedLi(currentFocusedLi, dropdownId) {
   // document.querySelector(`#${dropdownId} .value-list`).scrollTop = nextFocusedLi.offsetTop - 320;
   DropdownFocusedLisDict[dropdownId] = nextFocusedLi;
 
-  nextFocusedLi.scrollIntoViewIfNeeded();
+  nextFocusedLi.scrollIntoViewIfNeeded(false);
 }
 function setPrevFocusedLi(currentFocusedLi, dropdownId) {
   let prevFocusedLi = currentFocusedLi.previousElementSibling;
@@ -518,7 +518,7 @@ function setPrevFocusedLi(currentFocusedLi, dropdownId) {
   // document.querySelector(`#${dropdownId} .value-list`).scrollTop = prevFocusedLi.offsetTop - 30;
   DropdownFocusedLisDict[dropdownId] = prevFocusedLi;
 
-  prevFocusedLi.scrollIntoViewIfNeeded();
+  prevFocusedLi.scrollIntoViewIfNeeded(false);
 }
 
 function onDropdownItemClick(dropdownId, item) {
