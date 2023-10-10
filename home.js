fed3e77e541abd315ebc274ab3efab44aafdff76
Giannis Soumpasis <wweacountss@gmail.com>
2023-10-10 17:21:41 +0300

    webflow push

--- home.js
+++ home.js
@@ -186,64 +186,6 @@ document.addEventListener('DOMContentLoaded', () => {
   initUserInfo();
 });
 
-function initUserInfo() {
-  userInfo = getUserInfo() || {};
-  [...document.querySelectorAll('.user-info-username')].map(el => {
-    el.value = userInfo.username || '';
-    el.autocomplete = 'name';
-  });
-  [...document.querySelectorAll('.user-info-email')].map(el => {
-    el.value = userInfo.email || '';
-    el.autocomplete = 'email';
-  });
-  [...document.querySelectorAll('.user-info-phone')].map(el => {
-    el.value = userInfo.phone || '';
-    el.autocomplete = 'phone';
-  });
-  [...document.querySelectorAll('.user-info-address')].map(el => {
-    el.value = userInfo.address || '';
-    el.autocomplete = 'street-address';
-  });
-
-  [...document.querySelectorAll('.user-info-username')].map(element =>
-    element.addEventListener('input', e => {
-      [...document.querySelectorAll('.user-info-username')].map(el => {
-        el.value = e.target.value;
-      });
-      userInfo.username = e.target.value;
-      saveUserInfo();
-    })
-  );
-  [...document.querySelectorAll('.user-info-email')].map(element =>
-    element.addEventListener('input', e => {
-      [...document.querySelectorAll('.user-info-email')].map(el => {
-        el.value = e.target.value;
-      });
-      userInfo.email = e.target.value;
-      saveUserInfo();
-    })
-  );
-
-  [...document.querySelectorAll('.user-info-phone')].map(element =>
-    element.addEventListener('input', e => {
-      [...document.querySelectorAll('.user-info-phone')].map(el => {
-        el.value = e.target.value;
-      });
-      userInfo.phone = e.target.value;
-      saveUserInfo();
-    })
-  );
-  [...document.querySelectorAll('.user-info-address')].map(element =>
-    element.addEventListener('input', e => {
-      [...document.querySelectorAll('.user-info-address')].map(el => {
-        el.value = e.target.value;
-      });
-      userInfo.address = e.target.value;
-      saveUserInfo();
-    })
-  );
-}
-
 function saveUserInfo() {
   if (typeof Storage !== 'undefined')
     preferredStorage.setItem('userInfo', JSON.stringify(userInfo));
@@ -2513,6 +2455,9 @@ function initUserInfo() {
     el.value = userInfo.address || '';
     el.autocomplete = 'street-address';
   });
+  [...document.querySelectorAll('.user-info-region')].map(el => {
+    el.value = userInfo.region || 'ΑΤΤΙΚΗΣ';
+  });
   [...document.querySelectorAll('.user-info-installer')].map(el => {
     el.style.display = userInfo.installer ? 'block' : 'none';
   });
@@ -2551,6 +2496,15 @@ function initUserInfo() {
       el.value = e.target.value;
     });
     userInfo.address = e.target.value;
+    saveUserInfo();
+  })
+);
+[...document.querySelectorAll('.user-info-region')].map(element =>
+  element.addEventListener('change', e => {
+    [...document.querySelectorAll('.user-info-region')].map(el => {
+      el.value = e.target.value;
+    });
+    userInfo.region = e.target.value;
     saveUserInfo();
   })
 );
