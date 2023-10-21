a83d8dab1bd8da57fa7c817bb20dbea48defa60c
isoumpasis <wweacountss@gmail.com>
2023-10-22 01:30:05 +0300

    webflow push

--- home.js
+++ home.js
@@ -379,7 +379,8 @@ function initCardFiles() {
     file.removeAttribute('sizes');
   });
 }
-
+var xDown = null;
+var yDown = null;
 function initGalleryFiles() {
   document.querySelector('.gallery-wrap').addEventListener('click', () => {
     closeGallery();
@@ -434,6 +435,56 @@ function initGalleryFiles() {
       selectMainGalleryFile(galleryMainFileSelectedIndex + 1);
     }
   });
+
+  document.addEventListener('touchstart', handleTouchStart, false);
+  document.addEventListener('touchmove', handleTouchMove, false);
+
+  function getTouches(evt) {
+    return (
+      evt.touches || // browser API
+      evt.originalEvent.touches
+    ); // jQuery
+  }
+
+  function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];
+    xDown = firstTouch.clientX;
+    yDown = firstTouch.clientY;
+  }
+
+  function handleTouchMove(evt) {
+    if (!xDown || !yDown) {
+      return;
+    }
+
+    var xUp = evt.touches[0].clientX;
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if (Math.abs(xDiff) > Math.abs(yDiff)) {
+      /*most significant*/
+      if (xDiff > 0) {
+        /* right swipe */
+        console.log('right');
+      } else {
+        /* left swipe */
+        console.log('left');
+      }
+    } else {
+      if (yDiff > 0) {
+        /* down swipe */
+        console.log('down');
+      } else {
+        /* up swipe */
+        console.log('up');
+      }
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;
+  }
 }
 
 function openGallery(selectedIndex = 0) {
