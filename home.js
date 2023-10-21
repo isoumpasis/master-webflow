135af2a1d1997d34c012c3e667bff9df26082eb6
isoumpasis <wweacountss@gmail.com>
2023-10-21 17:15:20 +0300

    webflow push

--- home.js
+++ home.js
@@ -678,7 +678,7 @@ function initCustomDropdown({ dropdownId, placeholderStr }) {
     inputField.setSelectionRange(0, inputField.value.length);
 
     if (_isDropdownOpen()) {
-      // console.log('Dropdown is already open');
+      console.log('Dropdown is already open');
       inputField.setAttribute('inputmode', 'text');
       setTimeout(() => {
         inputField.focus();
