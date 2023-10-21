e699e487ffdb17ae82a96590719358473b52b067
isoumpasis <wweacountss@gmail.com>
2023-10-21 17:58:53 +0300

    webflow push

--- home.js
+++ home.js
@@ -348,7 +348,9 @@ function addToContainerPrice(amount) {
 }
 
 function initCardFiles() {
-  const cardMainFileContainers = [...document.querySelectorAll('.main-file video, .main-file.img')];
+  const cardMainFileContainers = [
+    ...document.querySelectorAll('.main-file video, .main-file .img')
+  ];
   cardMainFileContainers.forEach(c => {
     c.addEventListener('click', e => {
       e.preventDefault();
