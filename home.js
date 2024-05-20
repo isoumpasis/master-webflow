a94a20abf32b284d7b66254d631c395fd970b532
Giannis Soumpasis <wweacountss@gmail.com>
2024-05-20 16:15:11 +0300

    webflow push

--- home.js
+++ home.js
@@ -79,6 +79,12 @@ const SystemDict = {
       url: 'https://uploads-ssl.webflow.com/6423dc0021de6a2495a22761/649002d89900bc3e320cc6d3_sr-logo-02.svg',
       png: 'https://uploads-ssl.webflow.com/6423dc0021de6a2495a22761/6530da844f1e4949334e7694_sr-logo-02.png'
     },
+    SD: {
+      name: 'SD',
+      priceNoVAT: 1260,
+      url: 'https://uploads-ssl.webflow.com/6423dc0021de6a2495a22761/664b49e6ce20602f8a2d3f19_master-direct-sd.svg',
+      png: 'https://uploads-ssl.webflow.com/6423dc0021de6a2495a22761/664b49d6b2f33d7b21564fcb_master-direct-sd.png'
+    },
     AR: {
       name: 'AR',
       priceNoVAT: 1350,
